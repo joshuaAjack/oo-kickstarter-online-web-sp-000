078f75d1ebfb804903886bc41fd2ec98ab518807
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-13 01:25:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,8 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(backer)
-    @backer << @backed_projects
+  def back_project(title)
+    @title = @backed_projects
   end
   
 end 
